085539c3c57900e304c914cf89333443c4dda94a
TSWELOPELE09 <130665767+TSWELOPELE09@users.noreply.github.com>
2024-09-09 20:29:05 +0000

    Add files via upload

---
+++ Claim.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMCS.Models
+{
+    public class Claim
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Surname { get; set; }
+        public string Module { get; set; }
+
+      
+        public int Hours { get; set; }
+
+        public int Rates { get; set; }
+
+         
+    }
+}
+
+
